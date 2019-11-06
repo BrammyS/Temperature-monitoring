8367cac59be75ac55beb1e8903048d4c006a91de
BramEsendam <bram.esendam@gmail.com>
2019-11-06 23:39:59 +0100

    Added IMeasurementRepository

---
+++ TemperatureMonitor.Persistence/Repositories/IMeasurementRepository.cs
@@ -0,0 +1,13 @@
+﻿using TemperatureMonitor.Persistence.Domain.Collections;
+
+namespace TemperatureMonitor.Persistence.Repositories
+{
+
+    /// <summary>
+    /// This interface holds all the extra methods to query to the <see cref="Measurement"/> table/collection.
+    /// </summary>
+    public interface IMeasurementRepository : IRepository<Measurement>
+    {
+        
+    }
+}
