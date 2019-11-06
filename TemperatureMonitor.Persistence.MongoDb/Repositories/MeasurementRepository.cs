3a1639cc02e64358d425314dbbaea5fd9b6e259f
BramEsendam <bram.esendam@gmail.com>
2019-11-06 23:48:32 +0100

    Added implementation for IMeasurementRepository

---
+++ TemperatureMonitor.Persistence.MongoDb/Repositories/MeasurementRepository.cs
@@ -0,0 +1,13 @@
+﻿using TemperatureMonitor.Persistence.Domain.Collections;
+using TemperatureMonitor.Persistence.Repositories;
+
+namespace TemperatureMonitor.Persistence.MongoDb.Repositories
+{
+
+    public class MeasurementRepository : Repository<Measurement>, IMeasurementRepository
+    {
+        public MeasurementRepository(MongoContext context) : base(context, nameof(Measurement) + "s")
+        {
+        }
+    }
+}
