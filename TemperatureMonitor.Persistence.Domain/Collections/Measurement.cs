2d2aa117b0f80ecb5f8aceae9a2e04843d250b4d
BramEsendam <bram.esendam@gmail.com>
2019-11-06 23:35:45 +0100

    Added domain objects

---
+++ TemperatureMonitor.Persistence.Domain/Collections/Measurement.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace TemperatureMonitor.Persistence.Domain.Collections
+{
+
+    public class Measurement : Document
+    {
+
+        /// <summary>
+        /// Temperature in Celsius
+        /// </summary>
+        [BsonElement("Celsius")]
+        public double Celsius { get; set; }
+
+
+        /// <summary>
+        /// Temperature in Fahrenheit
+        /// </summary>
+        [BsonElement("Fahrenheit")]
+        public double Fahrenheit { get; set; }
+
+
+        /// <summary>
+        /// Temperature in Kelvin
+        /// </summary>
+        [BsonElement("Kelvin")]
+        public double Kelvin { get; set; }
+
+
+        /// <summary>
+        /// Humidity in percentage
+        /// </summary>
+        [BsonElement("Humidity")]
+        public double Humidity { get; set; }
+    }
+}
---
+++ TemperatureMonitor.Persistence.Domain/Document.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TemperatureMonitor.Persistence.Domain
+{
+
+    public class Document
+    {
+
+        /// <summary>
+        /// Creates a new <see cref="Document"/>
+        /// </summary>
+        public Document()
+        {
+            AddedAtUtc = DateTime.UtcNow;
+            BsonObjectId = ObjectId.GenerateNewId();
+        }
+
+
+        /// <summary>
+        /// The <see cref="ObjectId"/> of the document.
+        /// </summary>
+        [BsonId]
+        [BsonElement("_id")]
+        public ObjectId BsonObjectId { get; set; }
+
+
+        /// <summary>
+        /// The <see cref="DateTime"/> of when the <see cref="Document"/> was added to the collection.
+        /// </summary>
+        [BsonElement("AddedAtUtc")]
+        public DateTime AddedAtUtc { get; set; }
+    }
+}
