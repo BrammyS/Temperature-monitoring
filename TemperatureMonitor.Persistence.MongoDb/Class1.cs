fe7752e6beecbef6402c6ec5e239676730169a31
BramEsendam <bram.esendam@gmail.com>
2019-11-06 23:35:36 +0100

    Added projects

---
+++ TemperatureMonitor.Persistence.MongoDb/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TemperatureMonitor.Persistence.MongoDb
+{
+    public class Class1
+    {
+    }
+}
---
+++ TemperatureMonitor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TemperatureMonitor
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
