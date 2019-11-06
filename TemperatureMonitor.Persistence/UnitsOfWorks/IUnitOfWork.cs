8000bddfb5c161a721087a60839fb650123d9d0e
BramEsendam <bram.esendam@gmail.com>
2019-11-06 23:40:46 +0100

    Added  IUnitOfWork

---
+++ TemperatureMonitor.Persistence/UnitsOfWorks/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TemperatureMonitor.Persistence.Domain.Collections;
+using TemperatureMonitor.Persistence.Repositories;
+
+namespace TemperatureMonitor.Persistence.UnitsOfWorks
+{
+
+    /// <summary>
+    /// This UnitOfWork contains all the Repositories used to query the all the tables/collections.
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+
+        /// <summary>
+        /// Contains all the queries to the <see cref="Measurement"/> table/collection.
+        /// </summary>
+        IMeasurementRepository Logs { get; }
+    }
+}
