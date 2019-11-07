f98c50bb180d7e5d02893ae6bf7cd05717a152d8
BramEsendam <bram.esendam@gmail.com>
2019-11-07 14:55:48 +0100

    Delete ds18b20.py

--- Python/ds18b20.py
+++
@@ -1,8 +0,0 @@
-import time
-from w1thermsensor import W1ThermSensor
-sensor = W1ThermSensor()
-
-while True:
-    temperature = sensor.get_temperature()
-    print("The temperature is %s celsius" % temperature)
-    time.sleep(1)
