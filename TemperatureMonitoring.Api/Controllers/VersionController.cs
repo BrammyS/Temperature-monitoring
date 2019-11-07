ff2fa993ac98f9e210cf951506b21de5109fc05c
BramEsendam <bram.esendam@gmail.com>
2019-11-07 14:14:52 +0100

    Added /api/version

---
+++ TemperatureMonitoring.Api/Controllers/VersionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemperatureMonitoring.Api.Controllers
+{
+
+    [Route("api/version")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+
+        /// <summary>
+        /// Shows miscellaneous information about the api.
+        /// </summary>
+        /// <example>
+        /// GET: api/version
+        /// </example>
+        /// <returns>
+        /// Misc information about the api.
+        /// </returns>
+        [HttpGet]
+        public OkObjectResult Get()
+        {
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            var buildDate = new DateTime(2000, 1, 1)
+                            .AddDays(version.Build).AddSeconds(version.Revision * 2);
+            var displayableVersion = $"v{version} ({buildDate})";
+
+            return Ok($"Color chan api: {displayableVersion}");
+        }
+    }
+}
